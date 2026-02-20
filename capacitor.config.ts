59b4a2804440ef37c3bb58bee7831f93c6141255
Bilal Dar <darajazb@gmail.com>
2026-02-20 07:43:30 +0530

    Added Android APK setup and Black Screen Fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dartv.app',
   appName: 'DarTV',
-  webDir: 'dist'
+  webDir: 'dist',
+  // 👇 This is the magic part that unblocks the video streams 👇
+  server: {
+    cleartext: true,
+    allowNavigation: ['*']
+  }
 };
 
 export default config;
