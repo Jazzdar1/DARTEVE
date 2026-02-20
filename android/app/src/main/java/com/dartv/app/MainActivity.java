fd6278b47458a581f6385bf76d8d550305eb41a3
Bilal Dar <darajazb@gmail.com>
2026-02-20 07:12:16 +0530

    Added Android APK setup and Black Screen Fix

---
+++ android/app/src/main/java/com/dartv/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dartv.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dartv.app',
+  appName: 'DarTV',
+  webDir: 'dist'
+};
+
+export default config;
