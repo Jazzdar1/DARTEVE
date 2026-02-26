29899a1cbc7f61ec28dd2a50c4c349a2d7ef9694
Bilal Dar <darajazb@gmail.com>
2026-02-27 00:30:25 +0530

    Brief description of your changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.dartv.app',
   appName: 'DarTV',
-  webDir: 'dist', // Agar Vite hai to 'dist', warna 'build'
-  bundledWebRuntime: false,
-  plugins: {
-    CapacitorHttp: {
-      enabled: true, // 🚀 YEH ON HONA LAZMI HAI CORS BYPASS KE LIYE
-    },
-  },
-  server: {
-    cleartext: true, 
-    allowNavigation: ["*"]
-  },
-  android: {
-    allowMixedContent: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
